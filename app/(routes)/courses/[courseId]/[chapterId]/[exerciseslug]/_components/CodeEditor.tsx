1d1f9b00b7a1d673aa34fa6c777f4d09c49139b3
Ashish-029 <ashishran82@gmail.com>
2026-08-24 13:50:56 +0530

    Initial commit

---
+++ app/(routes)/courses/[courseId]/[chapterId]/[exerciseslug]/_components/CodeEditor.tsx
@@ -0,0 +1,125 @@
+'use client'
+
+import React from "react";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  useSandpack,
+} from "@codesandbox/sandpack-react";
+import SplitPane from "react-split-pane";
+import "react-split-pane/lib/styles.css";
+import { CourseExercise } from "../page";
+import { Button } from "@/components/ui/button";
+import { nightOwl } from "@codesandbox/sandpack-themes";
+import { useParams } from "next/navigation";
+import axios from "axios";
+import { toast } from "sonner";
+
+type Props = {
+  courseExerciseData: CourseExercise | undefined;
+  loading: boolean;
+};
+
+const CodeEditorChildren = ({ onCompleteExercise, IsCompleted }: any) => {
+  const { sandpack } = useSandpack();
+
+  return (
+    <div className="font-game absolute bottom-40 flex gap-5 right-5">
+      <Button
+        variant={"pixel"}
+        size={"lg"}
+        className="text-xl cursor-pointer"
+        onClick={() => sandpack.runSandpack()}
+      >
+        Run Code
+      </Button>
+      <Button
+        variant={"pixel"}
+        className="bg-[#a3e534] text-xl cursor-pointer"
+        size={"lg"}
+        onClick={() => onCompleteExercise()}
+        disabled={IsCompleted}
+      >
+        {IsCompleted ? 'Already Completed!' : 'Mark as Completed'}
+      </Button>
+    </div>
+  );
+};
+
+function CodeEditor({ courseExerciseData }: Props) {
+  const { exerciseslug } = useParams();
+
+  const exerciseIndex = courseExerciseData?.exercises?.findIndex(
+    (item) => item.slug == exerciseslug
+  );
+
+  const IsCompleted = courseExerciseData?.completedExercises?.find(
+    (item) => item?.exerciseId == Number(exerciseIndex) + 1
+  );
+
+  const onCompleteExercise = async () => {
+    if (exerciseIndex == undefined) return;
+
+    const result = await axios.post('/api/exercise/complete', {
+      courseId: courseExerciseData?.courseId,
+      chapterId: courseExerciseData?.chapterId,
+      exerciseId: exerciseIndex + 1,
+      xpEarned: courseExerciseData?.exercises[exerciseIndex].xp
+    });
+
+    console.log(result);
+    toast.success("Exercise Completed!");
+  };
+
+  return (
+    <div className="h-screen">
+      <SandpackProvider
+        //@ts-ignore
+        template={courseExerciseData?.editorType ?? "static"}
+        theme={nightOwl}
+        files={courseExerciseData?.exerciseData.exercisesContent.starterCode ?? {}}
+        options={{
+          autorun: false,
+          autoReload: false,
+        }}
+      >
+        <SandpackLayout style={{ height: "100%" }}>
+          
+          <SplitPane
+            split="vertical"
+            minSize={300}
+            defaultSize="50%"
+          >
+            {/* LEFT: Code Editor */}
+            <div className="relative h-full">
+              <SandpackCodeEditor
+                showTabs
+                style={{ height: "100%" }}
+              />
+              <CodeEditorChildren
+                onCompleteExercise={onCompleteExercise}
+                IsCompleted={IsCompleted}
+              />
+            </div>
+
+            {/* RIGHT: Preview */}
+            <div className="h-full">
+              <SandpackPreview
+                showNavigator
+                showOpenInCodeSandbox={false}
+                showOpenNewtab
+                style={{ height: "100%" }}
+              />
+            </div>
+
+          </SplitPane>
+
+        </SandpackLayout>
+      </SandpackProvider>
+    </div>
+  );
+}
+
+export default CodeEditor;
